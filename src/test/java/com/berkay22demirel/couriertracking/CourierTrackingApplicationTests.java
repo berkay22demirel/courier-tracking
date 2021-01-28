76dfa0bdc7372f6c7a190b90f170c880016d3684
berkay-demirel <berkay_demirel22@hotmail.com>
2021-01-29 01:00:59 +0300

    Initial commit

---
+++ src/test/java/com/berkay22demirel/couriertracking/CourierTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.couriertracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
